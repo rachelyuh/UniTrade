4ac6636001767fa750bac138074a6e583a1a6104
hchung313 <hchung313@gatech.edu>
2023-10-14 05:55:52 -0400

    Create Listing

---
+++ SwiftUIStarterKitApp/CreateListing.swift
@@ -0,0 +1,19 @@
+//
+//  CreateListing.swift
+//  SwiftUIStarterKitApp
+//
+//  Created by Hera Chung on 10/13/23.
+//  Copyright © 2023 NexThings. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateListing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateListing()
+}
---
+++ SwiftUIStarterKitApp/PaymentOptions.swift
@@ -0,0 +1,19 @@
+//
+//  PaymentOptions.swift
+//  SwiftUIStarterKitApp
+//
+//  Created by Hera Chung on 10/14/23.
+//  Copyright © 2023 NexThings. All rights reserved.
+//
+
+import SwiftUI
+
+struct PaymentOptions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentOptions()
+}
